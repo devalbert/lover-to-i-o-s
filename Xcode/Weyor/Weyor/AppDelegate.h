91130047337153501a0883bfdc06a1d83b5dbbc3
Albert Zhao <iphone.dev.albert@gmail.com>
2013-01-24 21:11:20 +0800

    Setup weyor app as initial status

---
+++ Xcode/Weyor/Weyor/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Weyor
+//
+//  Created by Albert Zhao on 1/24/13.
+//  Copyright (c) 2013 Albert Zhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
